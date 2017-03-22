3c15c95f5cc61cc4429edc06df83f38c097e7e4d
Ted-Koomen <koomen.ted@gmail.com>
2017-03-21 22:36:27 -0400

    add methods for happy and angry

--- ruby/shout_fiddlers.rb
+++ ruby/shout_fiddlers.rb
@@ -2,4 +2,11 @@ module Shout
 	def self.yell_angrily(words)
 		words + "!!!" + " :( "
 	end
+
+	def self.yell_happily(words)
+		words + "!!!" + " :) "
+	end
 end
+
+p Shout.yell_angrily("Damnit")
+p Shout.yell_happily("YAY")
